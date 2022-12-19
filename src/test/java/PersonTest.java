6beec6eb7de86b24891bd7e2027bd5e4a3a09296
Albert Ramos <vallvicity@hotmail.com>
2022-12-19 13:30:37 +0100

    Second commit: Un test puesto

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+
+public class PersonTest {
+
+    @Test
+    public void testName() {
+        Person pers = new Person(Juan);
+        String result = pers.getName();
+        assertEquals(Juan, result);
+    }
+}
